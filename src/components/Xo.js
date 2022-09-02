7ef17188bc5b3e467d1813e6c94bfda8b87bf83c
Saddam Hilles <sdm.hilles@gmail.com>
2022-09-02 11:59:44 +0300

    fix bug .. click on the cell

--- src/components/Xo.js
+++ src/components/Xo.js
@@ -29,12 +29,12 @@ const Xo = () => {
     }
   }, [info]);
 
-  const handleClick = (square) => {
+  const handleClick = (cell) => {
+    if (values[cell] !== "") return;
     const cloneArr = values.map((val, idx) => {
-      if (idx === square && val === "") {
+      if (idx === cell && val === "") {
         return player;
       }
-
       return val;
     });
     setValues(cloneArr);
@@ -43,7 +43,6 @@ const Xo = () => {
       alert("No winner!!!");
       setValues(initialValue);
     }
-    console.log("isFull: ", isFull);
   };
 
   const checkWin = () => {
@@ -56,7 +55,6 @@ const Xo = () => {
           foundWinningPattern = false;
         }
       });
-
       if (foundWinningPattern) {
         setInfo({ winner: player, state: "Won" });
       }
@@ -67,7 +65,7 @@ const Xo = () => {
     <div className="container">
       {values.map((cell, cellIndex) => (
         <div
-          className="req"
+          className="cell"
           key={String("index" + cellIndex)}
           onClick={() => handleClick(cellIndex)}
         >
@@ -79,3 +77,57 @@ const Xo = () => {
 };
 
 export default Xo;
+
+// import React, { useState } from "react";
+// const Patterns = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
+// const Xo = () => {
+//   const initialValue = [
+//     [null, null, null],
+//     [null, null, null],
+//     [null, null, null],
+//   ];
+//   const [val, setVal] = useState(initialValue);
+//   const [flag, setFlag] = useState(false);
+//   const handleClick = (rowIndex, cellIndex) => {
+//     let xo = "";
+//     console.log(`row: ${rowIndex} cell: ${cellIndex}`);
+//     const clone = [...val];
+//     if (flag) xo = "O";
+//     else xo = "X";
+
+//     clone[rowIndex][cellIndex] = xo;
+//     setVal(clone);
+//     setFlag((prev) => !prev);
+//     const isFull = val.every((firstDime) =>
+//       firstDime.every((secondDime) => secondDime !== null)
+//     );
+//     if (isFull) setVal(initialValue);
+//     console.log("isFull: ", isFull);
+//   };
+//   return (
+//     <div className="container">
+//       {val.map((row, rowIndex) =>
+//         row.map((cell, cellIndex) => (
+//           <div
+//             className="req"
+//             key={String(rowIndex + cellIndex)}
+//             onClick={() => handleClick(rowIndex, cellIndex)}
+//           >
+//             {cell}
+//           </div>
+//         ))
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Xo;
